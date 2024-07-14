8726bfb4a8aa806bae66ff5970801f3aa99335d0
Ramom Oliveira <ra-mom2000@hotmail.com>
2024-07-14 19:56:03 -0300

    first commit

---
+++ ex003/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Tipos primitivos em PHP</title>
+</head>
+
+<body>
+  <h1>Teste de tipos primitivos</h1>
+  <?php
+    // $num = 10;
+    // echo "A variável num é do tipo: $num <br>";
+
+    // var_dump($num) mostra tudo sobre a variável
+    // $v = 300;
+    // var_dump($v);
+
+    // $num = 3e2; // 3 x 10(2) tres vezes 10 elevado a 2
+    // echo "A variável num é do tipo: $num <br>";
+  ?>
+</body>
+
+</html>
